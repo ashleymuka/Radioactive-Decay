08d21e631117001776a595837d1741cbb7107c32
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 23:11:56 -0600

    Update radioactivedecay.py

--- radioactivedecay.py
+++ radioactivedecay.py
@@ -1,17 +1,9 @@
 '''
-Author: Ashley Muka
-Assignment Title: Radioactive decay
-Assignment Description:complete function to implement formulas
-Due Date:09/29/2023
-Date Created:09/27/2023
-Date Last Modified:09/28/2023
-
+Description: complete function to implement formula for radioactive decay
 '''
 
-#data abstraction
 import math
 
-#process
 def compute_Nt(NO, t, T):
     
     Nt = NO * math.e ** (-0.693 * t / T)
@@ -25,26 +17,22 @@ def compute_T(NO, Nt, t):
 
 
 if __name__ == "__main__":
-
-#input
     
     choice = input()
     NO = float(input())
     t = float(input())
     T = float(input())
 
-#process and output    
+
     if choice == "N":
         
         Nt = compute_Nt(NO, t, T)
-
         print(f'Nt = {Nt:.4f}')
 
 
     elif choice == "T":
 
         T = compute_T(NO, Nt, t)
-
         print(f'T = {T:.4f}')
 
         
